138eb67b776763d2ac426ad8624861e73e356076
Shirley <1493008661@qq.com>
2020-10-20 22:59:56 +0800

    Fourth Assignment
    A cloth & break(explode) practice

---
+++ Fourth/Assets/Script/Grenade.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3f;
+    public float radius = 5f;
+    public float force = 700f;
+    public GameObject explosionEffect;
+    bool hasExploded = false;
+
+    float countdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f&&!hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, radius);
+
+
+        foreach(Collider nearbyObject in collidersToDestroy)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+
+            Destructible dest= nearbyObject.GetComponent<Destructible>();
+            if (dest != null)
+            {
+                dest.Destroy();
+
+            }
+
+        }
+
+        Collider[] collidersToMove = Physics.OverlapSphere(transform.position, radius);
+        foreach(Collider nearbyObject in collidersToMove)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Fourth/Assets/Script/SwitchScene.cs
+++ Fourth/Assets/Script/SwitchScene.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SwitchToCloth()
     {
-        
+        SceneManager.LoadScene(0);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SwitchToBreak()
     {
-        
+        SceneManager.LoadScene(1);
     }
 }
