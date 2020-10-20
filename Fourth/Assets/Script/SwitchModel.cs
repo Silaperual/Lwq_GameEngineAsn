da0391ecabf95b30282b0fef2fc1c8ba257e5155
Shirley <1493008661@qq.com>
2020-10-20 16:33:19 +0800

    Fourth Assignment
    Cloth & Break(explode) effects

---
+++ Fourth/Assets/Script/SwitchModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchModel : MonoBehaviour
+{
+    public GameObject GameObjC;
+    public GameObject GameObjS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClothToSpring()
+    {
+        GameObjC.SetActive(false);
+        GameObjS.SetActive(true);
+    }
+    public void SpringToCloth()
+    {
+        GameObjC.SetActive(true);
+        GameObjS.SetActive(false);
+    }
+}
