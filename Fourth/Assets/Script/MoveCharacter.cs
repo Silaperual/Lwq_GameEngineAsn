13ec0bee94c1c124de75ee7854b1a1c5d0b7fa2a
Shirley <1493008661@qq.com>
2020-10-21 17:04:20 +0800

    Fourth Assignment
    A cloth & break(explode) practice

---
+++ Fourth/Assets/Script/MoveCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 curCharacterPosition = transform.position;
+
+        if (count < 300)
+        {
+            Vector3 movedPosition = new Vector3(curCharacterPosition.x + 0.0008f, curCharacterPosition.y, curCharacterPosition.z);
+            transform.position = movedPosition;
+            count++;
+        }
+
+        if (count >= 300 && count < 600)
+        {
+            Vector3 movedPosition = new Vector3(curCharacterPosition.x - 0.0008f, curCharacterPosition.y, curCharacterPosition.z);
+            transform.position = movedPosition;
+            count++;
+        }
+        if (count == 600)
+        {
+            count = 0;
+        }
+    }
+}
