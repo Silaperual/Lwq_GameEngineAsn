9ed5f6e56ad4f7857c370817bf31642b7cd9ebad
Shirley <1493008661@qq.com>
2020-12-10 03:06:53 +0800

    Nineth Assignment

---
+++ Nineth/Assets/Script/PlayUISound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayUISound : MonoBehaviour
+{
+    public string eventName = null;
+    // Start is called before the first frame update
+
+    public void Play()
+    {
+        GameObject mainCamera = GameObject.Find("Main Camera");
+        if (!string.IsNullOrEmpty(eventName))
+        {
+            AkSoundEngine.PostEvent(eventName, mainCamera);
+        }
+    }
+}
